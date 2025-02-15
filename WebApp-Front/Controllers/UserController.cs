79fe6d51c6284ccaf3c57341993fdae84c2ec109
nxj1129 <niko.jugovac@gmail.com>
2025-02-15 17:55:55 +0100

    WebApp Architecture

---
+++ WebApp-Front/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Front.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
